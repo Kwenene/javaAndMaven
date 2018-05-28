6e9e699bc36016b48b1cca1366606f2418043fe2
Kwenene <35527444+Kwenene@users.noreply.github.com>
2018-05-28 07:10:30 +0200

    Update node.groovy

--- complete/groovy/node.groovy
+++ complete/groovy/node.groovy
@@ -1,11 +1,11 @@
 import com.microsoft.jenkins.containeragents.builders.*
 
 def myCloud = new KubernetesCloudBuilder()
-    .withCloudName("mycloud")
-    .withAzureCredentialsId("<Azure Credentials Id>")
-    .withResourceGroup("myResourceGroup")
-    .withServiceName("myServiceName")
-    .withAcsCredentialsId("<ACS Credentials Id>")
+    .withCloudName("AzureCloud")
+    .withAzureCredentialsId("73620b42-8194-4319-9506-95f687811647")
+    .withResourceGroup("mavenResourceGroup")
+    .withServiceName("mavenProject")
+    .withAcsCredentialsId("5495a590-0146-4c9f-b21f-06e3a1fdf0c0")
     .addNewTemplate()
         .withName("mytemplate")
         .withLabel("k8s")
